26d9ecc7f0d4da5b529acab3221d1ff333069dd8
harun <harun.khairy@gmail.com>
2020-06-11 10:43:43 +0700

    Updates

---
+++ precache-manifest.d083b523375ce2e5dfb4091cd39e8ddc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2514be64ad9166237d2c3d43bcb22a3",
+    "url": "/monster-jc12/index.html"
+  },
+  {
+    "revision": "cd1f03a286d7742fa97c",
+    "url": "/monster-jc12/static/css/main.f761ac06.chunk.css"
+  },
+  {
+    "revision": "544168180dfdecf70c64",
+    "url": "/monster-jc12/static/js/2.df159f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-jc12/static/js/2.df159f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd1f03a286d7742fa97c",
+    "url": "/monster-jc12/static/js/main.56310f43.chunk.js"
+  },
+  {
+    "revision": "ee0f2ac6f9e22dbb0e1e",
+    "url": "/monster-jc12/static/js/runtime-main.94e6080b.js"
+  }
+]);
